30f9e7cd1defc2f0f1f9d52c14ddd6f9bd9f2b47
Паршин Александр <aleksandr.pashin721@gmail.com>
2022-06-17 22:13:25 +0300

    InceptionPoint

---
+++ src/main/java/com/ene/lipenergy/LipEnergyWebApplication.java
@@ -0,0 +1,13 @@
+package com.ene.lipenergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LipEnergyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LipEnergyWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ene/lipenergy/LipEnergyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ene.lipenergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LipEnergyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
