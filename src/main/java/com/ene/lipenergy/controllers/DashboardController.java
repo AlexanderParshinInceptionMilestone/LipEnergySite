1c473c20ec47ebd8d734a775da34958640eabf3b
Паршин Александр <aleksandr.pashin721@gmail.com>
2022-09-16 23:54:20 +0300

    js/css

--- src/main/java/com/ene/lipenergy/controllers/DashboardController.java
+++ src/main/java/com/ene/lipenergy/controllers/DashboardController.java
@@ -5,11 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class DashboardController {
 
-    @GetMapping("/")
+    @GetMapping("/dashboard")
     public String home(Model model) {
         model.addAttribute("title", "Главная страница");
-        return "home";
+        return "dashboard";
     }
 }
