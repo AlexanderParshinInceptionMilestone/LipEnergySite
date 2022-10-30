54e310f9e253cc698e83eb4d42b325d70fda67ab
Паршин Александр <aleksandr.pashin721@gmail.com>
2022-10-30 14:33:07 +0300

    30.10.2022 backUp

--- src/main/resources/static/js/visualizer.js
+++ src/main/resources/static/js/visualizer.js
@@ -2,10 +2,17 @@
     audio.crossOrigin = "anonymous";
     let canvas = document.querySelector("canvas");
 
-    canvas.width = window.innerWidth * 0.8;
-    canvas.height = window.innerHeight * 0.8;
+//    var parent = canvas.parentNode;
+//    canvas.width = parent.offsetWidth;
+//    canvas.height = parent.offsetHeight;
 
     let wave = new Wave(audio, canvas);
+
+//    wave.addAnimation(new wave.animations.Wave({
+//        lineWidth: 10,
+//        lineColor: "red",
+//        count: 20
+//    }));
 
       wave.addAnimation(
       new wave.animations.Wave({
@@ -18,52 +25,59 @@
         frequencyBand: "base",
       })
     );
-    wave.addAnimation(
-      new wave.animations.Cubes({
-        top: true,
-        count: 60,
-        cubeHeight: 10,
-        fillColor: "#FFFF00",
-        lineColor: "rgba(0,0,0,0)",
-        radius: 1,
-      })
-    );
-    wave.addAnimation(
-      new wave.animations.Circles({
-        lineColor: { gradient: ["#FF9A8B", "#FF7A88", "#FF99AC"], rotate: 90 },
-        lineWidth: 4,
-        diameter: 20,
-        count: 20,
-        frequencyBand: "base",
-      })
-    );
-    wave.addAnimation(
-      new wave.animations.Glob({
-        fillColor: { gradient: ["#FF9A8B", "#FF7A88", "#FF99AC"], rotate: 90 },
-        lineColor: "white",
-        glow: { strength: 15, color: "#FAD961" },
-        lineWidth: 10,
-        count: 45,
-      })
-    );
-    wave.addAnimation(
-      new wave.animations.Shine({
-        lineColor: "#FAD961",
-        glow: { strength: 15, color: "#FAD961" },
-        diameter: 300,
-        lineWidth: 10,
-      })
-    );
-    wave.addAnimation(
-      new wave.animations.Square({
-        lineColor: { gradient: ["#21D4FD", "#B721FF"] },
-      })
-    );
-    wave.addAnimation(
-      new wave.animations.Arcs({
-        lineWidth: 4,
-        lineColor: { gradient: ["#21D4FD", "#B721FF"] },
-        diameter: 500,
-        fillColor: { gradient: ["#21D4FD", "#21D4FD", "#B721FF"], rotate: 45 },
-      })
-    );
+//    wave.addAnimation(
+//      new wave.animations.Cubes({
+//        top: true,
+//        count: 60,
+//        cubeHeight: 10,
+//        fillColor: "#FFFF00",
+//        lineColor: "rgba(0,0,0,0)",
+//        radius: 1,
+//      })
+//    );
+//    wave.addAnimation(
+//      new wave.animations.Circles({
+//        lineColor: { gradient: ["#FF9A8B", "#FF7A88", "#FF99AC"], rotate: 90 },
+//        lineWidth: 4,
+//        diameter: 20,
+//        count: 20,
+//        frequencyBand: "base",
+//      })
+//    );
+//    wave.addAnimation(
+//      new wave.animations.Glob({
+//        fillColor: { gradient: ["#FF9A8B", "#FF7A88", "#FF99AC"], rotate: 90 },
+//        lineColor: "white",
+//        glow: { strength: 15, color: "#FAD961" },
+//        lineWidth: 10,
+//        count: 45,
+//      })
+//    );
+//    wave.addAnimation(
+//      new wave.animations.Shine({
+//        lineColor: "#FAD961",
+//        glow: { strength: 15, color: "#FAD961" },
+//        diameter: 300,
+//        lineWidth: 10,
+//      })
+//    );
+//    wave.addAnimation(
+//      new wave.animations.Square({
+//        lineColor: { gradient: ["#21D4FD", "#B721FF"] },
+//      })
+//    );
+//    wave.addAnimation(
+//      new wave.animations.Arcs({
+//        lineWidth: 4,
+//        lineColor: { gradient: ["#21D4FD", "#B721FF"] },
+//        diameter: 500,
+//        fillColor: { gradient: ["#21D4FD", "#21D4FD", "#B721FF"], rotate: 45 },
+//      })
+//    );
+
+//      window.addEventListener('resize', resizeCanvas, false);
+//
+//      function resizeCanvas() {
+//      canvas.width = parent.offsetWidth;
+//      canvas.height = parent.offsetHeight;
+//      }
